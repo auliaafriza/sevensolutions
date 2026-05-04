7ca0d69e5628ec23f2c5091a2077213ef92ea99d
aulia <auliaafriza@gmail.com>
2026-05-04 13:15:14 +0700

    seven solution frontend testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,110 @@
-import Image from "next/image";
+"use client";
+import { data } from "./dummy";
+import { useState } from "react";
+
+interface TypeSelect {
+  type: string;
+  name: string;
+}
 
 export default function Home() {
+  const [dataList, setDataList] = useState<TypeSelect[]>(data);
+  const [fruits, setFruits] = useState<TypeSelect[]>([]);
+  const [vegetable, setVegetable] = useState<TypeSelect[]>([]);
+  const [isFruitTurn, setIsFruitTurn] = useState(true);
+
+  const selectData = (items: TypeSelect) => {
+    if (items.type.toLowerCase() === "fruit") {
+      setFruits([...fruits, items]);
+      setDataList(dataList.filter((item) => item.name !== items.name));
+    } else if (items.type.toLowerCase() === "vegetable") {
+      setVegetable([...vegetable, items]);
+      setDataList(dataList.filter((item) => item.name !== items.name));
+    }
+  };
+  
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleRightClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    if (isFruitTurn) {
+      setFruits((prev) => {
+        if (!prev.length) return prev;
+
+        const removed = prev[0];
+
+        setDataList((dataList) =>
+          dataList.filter((item) => item.name !== removed.name).concat(removed),
+        );
+
+        return prev.slice(1);
+      });
+    } else {
+      setVegetable((prev) => {
+        if (!prev.length) return prev;
+
+        const removed = prev[0];
+
+        setDataList((dataList) =>
+          dataList.filter((item) => item.name !== removed.name).concat(removed),
+        );
+
+        return prev.slice(1);
+      });
+    }
+
+    setIsFruitTurn((prev) => !prev);
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <div
+      className="flex flex-col flex-1 items-center justify-center bg-white font-sans"
+      onContextMenu={handleRightClick}
+    >
+      <div className="grid grid-cols-3 gap-4">
+        <div className="border rounded flex flex-col">
+          {dataList.map((item, index) => (
+            <button
+              key={index}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded p-2 m-2"
+              onClick={() => selectData(item)}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              {item.name}
+            </button>
+          ))}
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="border rounded flex flex-col">
+          <div className="bg-gray-300 text-white text-center font-bold p-3">
+            Fruits
+          </div>
+          {fruits && fruits.length
+            ? fruits.map((item, index) => (
+                <button
+                  key={index}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded p-2 m-2"
+                >
+                  {item.name}
+                </button>
+              ))
+            : null}
         </div>
-      </main>
+        <div className="border rounded flex flex-col">
+          <div className="bg-gray-300 text-white text-center font-bold p-3">
+            Vegetables
+          </div>
+          {vegetable && vegetable.length
+            ? vegetable.map((item, index) => (
+                <button
+                  key={index}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded p-2 m-2"
+                >
+                  {item.name}
+                </button>
+              ))
+            : null}
+        </div>
+      </div>
     </div>
   );
 }
